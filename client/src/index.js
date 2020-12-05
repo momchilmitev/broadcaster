e996d56bb2a695ccc274f3989cf73c193091b33d
momchilmitev <momchilmitev87@gmail.com>
2020-12-05 16:35:09 +0100

    added second install and basic file scaffolding couse of missing assets in client folder

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
 
-const root = document.getElementById('root');
+import App from './components/App';
+const root = document.querySelector('#root');
 
 ReactDOM.render(<App />, root);
