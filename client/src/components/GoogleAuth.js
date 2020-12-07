bb1920b85ff7cd34d77d0b2cf7762afdb3664bc6
momchilmitev <momchilmitev87@gmail.com>
2020-12-07 18:48:01 +0100

    added basic skeleton of google auth component and importing it into the header

---
+++ client/src/components/GoogleAuth.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class GoogleAuth extends Component {
+  render() {
+    return <div>GoogleAuth</div>;
+  }
+}
--- client/src/index.js
+++ client/src/index.js
@@ -3,5 +3,7 @@ import ReactDOM from 'react-dom';
 
 import App from './components/App';
 const root = document.querySelector('#root');
+const apiKey =
+  '114735155429-5dft50fqsdll16230p6dthqghdhhvhit.apps.googleusercontent.com';
 
 ReactDOM.render(<App />, root);
