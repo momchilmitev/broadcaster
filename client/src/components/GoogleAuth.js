28245ee2ccab90644884cc7cb24d0a7090413c1a
momchilmitev <momchilmitev87@gmail.com>
2020-12-07 19:42:25 +0100

    added the logic for sign in and out with mark up for the coresponding buttons

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,17 +1,57 @@
 import React, { Component } from 'react';
 
 export default class GoogleAuth extends Component {
+  state = { isSignedIn: null };
+
   componentDidMount() {
     window.gapi.load('client:auth2', () => {
-      window.gapi.client.init({
-        clientId:
-          '114735155429-5dft50fqsdll16230p6dthqghdhhvhit.apps.googleusercontent.com',
-        scope: 'email',
-      });
+      window.gapi.client
+        .init({
+          clientId:
+            '114735155429-5dft50fqsdll16230p6dthqghdhhvhit.apps.googleusercontent.com',
+          scope: 'email',
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+          this.auth.isSignedIn.listen(this.onAuthChange);
+        });
     });
   }
 
+  onAuthChange = () => {
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  };
+
+  onSignIn = () => {
+    this.auth.signIn();
+  };
+
+  onSignOut = () => {
+    this.auth.signOut();
+  };
+
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return null;
+    } else if (this.state.isSignedIn) {
+      return (
+        <button onClick={this.onSignOut} className="ui red google button">
+          <i className="google icon" />
+          Sign out
+        </button>
+      );
+    } else {
+      return (
+        <button onClick={this.onSignIn} className="ui red google button">
+          <i className="google icon" />
+          Sign in
+        </button>
+      );
+    }
+  }
+
   render() {
-    return <div>GoogleAuth</div>;
+    return <div>{this.renderAuthButton()}</div>;
   }
 }
