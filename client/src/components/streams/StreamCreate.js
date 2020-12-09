2cdcddb79998bf6f7a837346444bc495f896f208
momchilmitev <momchilmitev87@gmail.com>
2020-12-09 20:13:46 +0100

    added axios, redux-thunk, create stream action creator and wiring all up

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import { createStream } from '../../actions';
 
 class StreamCreate extends React.Component {
   renderError({ error, touched }) {
@@ -22,7 +24,9 @@ class StreamCreate extends React.Component {
     );
   };
 
-  onSubmit(formValues) {}
+  onSubmit = (formValues) => {
+    this.props.createStream(formValues);
+  };
 
   render() {
     return (
@@ -56,7 +60,9 @@ const validate = (formValues) => {
   return errors;
 };
 
-export default reduxForm({
+const formWrapper = reduxForm({
   form: 'streamCreate',
   validate,
 })(StreamCreate);
+
+export default connect(null, { createStream })(formWrapper);
